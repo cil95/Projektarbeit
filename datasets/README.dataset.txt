dfc6f445cf0aecd682cde918df04695e7d1812b3
cil95 <can.ilgin@gmx.de>
2024-11-13 22:53:47 +0100

    create Project

---
+++ datasets/README.dataset.txt
@@ -0,0 +1,6 @@
+# InletFinder > version1inlet
+https://universe.roboflow.com/projektarbeitcanundharun/inletfinder-keaha
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main_image.py
@@ -0,0 +1,38 @@
+"Yolo ohne api von roboflow aber nur bilderkknunng keine webcam"
+from ultralytics import YOLO
+import cv2
+image = cv2.imread("inlet3.jpg")
+# Load the YOLO model
+model = YOLO("runs/detect/train2/weights/best.pt")
+
+# Run predictions on the image
+results = model.predict("inlet3.jpg")
+
+# Extract and print the bounding box data
+for result in results:
+    for box in result.boxes:
+        # Get the x1, y1 (top-left) and x2, y2 (bottom-right) coordinates
+        x1, y1, x2, y2 = box.xyxy[0]  # Get coordinates of the first detected object
+        # die texte
+        confidence = box.conf[0]  # Confidence score
+        class_id = int(box.cls[0])  # Class label
+        class_name=result.names[class_id]
+        label = f"Class {class_name}: {confidence:.2f} %"
+        print(label)
+        if class_name == "inlet":
+            cv2.rectangle(image,(int(x1),int(y1)),(int(x2),int(y2)),(255,255,255),2)
+            cv2.putText(image, "Inlet", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        else:
+            # Calculate width and height
+            width = int(x2) - int(x1)
+            height = int(y2) - int(y1)
+            radius = int((width+height)/4)
+            x = int(width/2)+int(x1)
+            y=int(height/2)+int(y1)
+            cv2.circle(image,(x,y),radius,(0,255,125),1)
+
+
+cv2.imshow("Detection",image)
+cv2.waitKey(0)
+
